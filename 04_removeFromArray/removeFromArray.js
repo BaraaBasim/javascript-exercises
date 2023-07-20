942042f56e626783173f491ae76daaa7c31eaea6
Baraa Basim <barabasim77@gmail.com>
2023-07-20 21:36:04 +0800

    Remove from array using filter()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,18 @@
 const removeFromArray = function(array, ...numbers) {
-    let newArry = [];
-    array.forEach((item) => {
-        if (!numbers.includes(item)){
-            newArry.push(item)
-        }
-    })
-    return newArry;
+    // let newArry = [];
+    // array.forEach((item) => {
+    //     if (!numbers.includes(item)){
+    //         newArry.push(item)
+    //     }
+    // })
+    // return newArry;
+
+
+
+    // Using filter()
+    return array.filter((item) => !numbers.includes(item))
+    
+    
  };
 
 // Do not edit below this line
