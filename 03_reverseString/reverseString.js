ede1c60d2674044fa7bef0e675ef1c74f480ca51
Baraa Basim <barabasim77@gmail.com>
2023-07-20 20:52:23 +0800

    reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    // Using built-in methods
+    // return word.split("").reverse().join("");
+
+
+    // Using a for loop
+    // let str = "";
+    // for (let i = word.length - 1; i >= 0; i--){
+    //     str += word[i];
+    // }
+    // return str;
+
+
+    // Using Recursion
+    if (word === "")
+        return ""
+
+    else 
+        return reverseString(word.substr(1)) + word.charAt(0)
 };
 
 // Do not edit below this line
